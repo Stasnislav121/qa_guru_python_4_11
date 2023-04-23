796067e4d9292e7bcd88e7054be2765e1a0de29a
Stanislav <oz3r@mail.ru>
2023-04-23 03:07:27 +0300

    add allure steps

--- demoqa/tests/test_form_registration.py
+++ demoqa/tests/test_form_registration.py
@@ -1,40 +1,45 @@
+import allure
 from selene import browser
 from demoqa.pages.registration_page import StudentRegistrationPage
 from demoqa.utils import attach
 
 
+@allure.title("Заполнение формы регистрации студенты")
 def test_registration_form_(browser_setup):
     registration_page = StudentRegistrationPage()
-    registration_page.open()
+    with allure.step("Открытие сайта"):
+        registration_page.open()
 
     # WHEN
-    registration_page.fill_first_name('Ivan')
-    registration_page.fill_last_name('Petrov')
-    registration_page.fill_email('petrov@abc.com')
-    registration_page.select_gender('Male')
-    registration_page.fill_mobile('7123456789')
-    registration_page.fill_date_of_birth("1917", "January", "5")
-    registration_page.fill_subjects('Maths')
-    registration_page.select_hobbies('Sports')
-    registration_page.upload_picture('one.png')
-    registration_page.fill_current_address('Rome, Italy')
-    registration_page.select_state('Uttar Pradesh')
-    registration_page.select_city('Agra')
-    registration_page.submit()
+    with allure.step("Заполнение данными"):
+        registration_page.fill_first_name('Ivan')
+        registration_page.fill_last_name('Petrov')
+        registration_page.fill_email('petrov@abc.com')
+        registration_page.select_gender('Male')
+        registration_page.fill_mobile('7123456789')
+        registration_page.fill_date_of_birth("1917", "January", "5")
+        registration_page.fill_subjects('Maths')
+        registration_page.select_hobbies('Sports')
+        registration_page.upload_picture('one.png')
+        registration_page.fill_current_address('Rome, Italy')
+        registration_page.select_state('Uttar Pradesh')
+        registration_page.select_city('Agra')
+        registration_page.submit()
 
     # THEN
-    registration_page.should_have_registered_user_with(
-        'Ivan Petrov',
-        'petrov@abc.com',
-        'Male',
-        '7123456789',
-        '05 January,1917',
-        'Maths',
-        'Sports',
-        'one.png',
-        'Rome, Italy',
-        'Uttar Pradesh Agra'
-    )
+    with allure.step("Проверка результата"):
+        registration_page.should_have_registered_user_with(
+            'Ivan Petrov',
+            'petrov@abc.com',
+            'Male',
+            '7123456789',
+            '05 January,1917',
+            'Maths',
+            'Sports',
+            'one.png',
+            'Rome, Italy',
+            'Uttar Pradesh Agra'
+        )
     attach.add_html(browser)
     attach.add_screenshot(browser)
     attach.add_logs(browser)
